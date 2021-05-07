0efb2cf1054ae029831e111791841f7586cd4664
RamandeepBawa <83805152+RamandeepBawa@users.noreply.github.com>
2021-05-07 12:18:05 +0530

    Update StringCalculator.java
    Done for simple Test Case

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,14 +1,17 @@
 class StringCalculator{
-  public int Add(String numbers){
+  public static int Add(String numbers){
     if(numbers.length()==0)
        return 0;
     else{
-      int[] arr=numbers.split(",");
+      String[] arr=numbers.split(",");
       int sum=0;
       for(int i=0;i<arr.length;i++){
-        sum=sum+a[i];
+        sum=sum+Integer.parseInt(arr[i]);
       }
       return sum;
     }
+  }
+  public static void main(String []args){
+    System.out.println(Add("1,2"));
   }
 }
