8a8011b4610f792377551747433bdf9f480382b7
RamandeepBawa <83805152+RamandeepBawa@users.noreply.github.com>
2021-05-07 12:50:13 +0530

    negative number exception part Complete
    negative number exception part Complete

--- StringCalculator.java
+++ StringCalculator.java
@@ -4,14 +4,30 @@ class StringCalculator{
        return 0;
     else{
       String del=",";
-      if(numbers.matches("//(.*)\n(.*)"){
+      if(numbers.matches("//(.*)\n(.*)")){
         del = Character.toString(numbers.charAt(2));
 				numbers = numbers.substring(4);
       }
       String[] arr=numbers.split(del + "|\n");
       int sum=0;
+      String negative=""; 
+	
       for(int i=0;i<arr.length;i++){
-        sum=sum+Integer.parseInt(arr[i]);
+	int temp=Integer.parseInt(arr[i]);
+	if(temp<0){
+		if(negative.length()==0){
+			negative=arr[i];
+		}
+		else{
+			negative = negative + "," + arr[i];
+		}
+	}
+	if(temp<1000){
+	        sum=sum+temp;
+	}
+	if(negative.length()>0){
+		throw new IllegalArgumentException("Negatives not allowed: " + negative);
+	}
       }
       return sum;
     }
