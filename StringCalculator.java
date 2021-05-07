8ec42847cc4e766fc9d9813af56fa87c4f6d598b
RamandeepBawa <83805152+RamandeepBawa@users.noreply.github.com>
2021-05-07 12:02:09 +0530

    Update StringCalculator.java
    Commit for simple Cases
    “” == 0 , “1” == 1 , “1,2” == 3

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,1 +1,14 @@
-
+class StringCalculator{
+  public int Add(String numbers){
+    if(numbers.length()==0)
+       return 0;
+    else{
+      int[] arr=numbers.split(",");
+      int sum=0;
+      for(int i=0;i<arr.length;i++){
+        sum=sum+a[i];
+      }
+      return sum;
+    }
+  }
+}
