139b571b94afadbfe65635caeed2dcff57e670e9
RamandeepBawa <83805152+RamandeepBawa@users.noreply.github.com>
2021-05-07 12:41:05 +0530

    Added Different Delimeter Case
    Added Different Delimeter Case

--- StringCalculator.java
+++ StringCalculator.java
@@ -3,7 +3,12 @@ class StringCalculator{
     if(numbers.length()==0)
        return 0;
     else{
-      String[] arr=numbers.split(",");
+      String del=",";
+      if(numbers.matches("//(.*)\n(.*)"){
+        del = Character.toString(numbers.charAt(2));
+				numbers = numbers.substring(4);
+      }
+      String[] arr=numbers.split(del + "|\n");
       int sum=0;
       for(int i=0;i<arr.length;i++){
         sum=sum+Integer.parseInt(arr[i]);
